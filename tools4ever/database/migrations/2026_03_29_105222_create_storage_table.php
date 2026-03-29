aeb37d893b8bf00e3ac9ce32442ff83e03ec1a81
Thecodingpanda <RafWoud@pm.me>
2026-03-29 21:24:33 +0200

    Moved around table variables

--- tools4ever/database/migrations/2026_03_29_105222_create_storage_table.php
+++ tools4ever/database/migrations/2026_03_29_105222_create_storage_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
             $table->id('storage_id');
             $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('location_id');
-            $table->foreign('product_id')->references('product_id')->on('product');
-            $table->foreign('location_id')->references('location_id')->on('location');
             $table->integer('amount');
             $table->integer('minumum_amount');
             $table->timestamps();
+            $table->foreign('product_id')->references('product_id')->on('product');
+            $table->foreign('location_id')->references('location_id')->on('location');
             $table->unique(['product_id', 'location_id']);
         });
     }
